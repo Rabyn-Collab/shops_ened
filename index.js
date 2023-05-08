888697c9d146bfd634ef773ecf56e9754861b377
Rabyn-Collab <babynshrestha64@gmail.com>
2023-05-08 08:22:32 +0545

    changes

--- index.js
+++ index.js
@@ -4,13 +4,14 @@ const app = express();
 const mongoose = require('mongoose');
 const port = 5000;
 const userRoutes = require('./routes/userRoutes');
-
+const cors = require('cors');
 
 mongoose.connect('mongodb+srv://rabin900:souls900@mycluster.emabxgd.mongodb.net/Shops').then((result) => {
   app.listen(port);
 }).catch((err) => {
   console.log(err);
 })
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.get('/', (req, res) => {
